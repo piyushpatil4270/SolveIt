e81e9c511ebde5dccdbdd35c81da72808f4009a7
Piyush <piyushpatil4270@gmail.com>
2024-01-03 23:28:38 +0530

    created state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,30 @@ import { SignIn } from "./components/SignIn";
 import { getAuth,onAuthStateChanged } from "firebase/auth";
 import { app } from "./utils/firebase";
 import { useEffect } from "react";
-import firebase from "firebase/compat/app";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { userAtom } from "./store/atoms/user";
 export const auth=getAuth(app)
 function App() {
-
+const [user,setUser]=useRecoilState(userAtom)
 
 useEffect(()=>{
 onAuthStateChanged(auth,(user) => { 
-  if (user) { 
+  if (user && user.email) { 
    // User is signed in 
+   setUser({
+    loading:false,
+    user:{
+      email:user.email
+    }
+   })
       console.log("User Signed In"); 
       var uid = user.uid; 
    } else { 
       // User is signed out 
-      console.log("User Signed Out"); 
+      setUser({
+        loading:false
+      })
+      console.log("There is no logged-in User"); 
       // ... 
   } 
 });
