3627d77a36476f72b72ddad3abe91d37cd58bb04
Piyush <piyushpatil4270@gmail.com>
2024-01-03 23:48:54 +0530

    created a navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,43 +5,61 @@ import { SignIn } from "./components/SignIn";
 import { getAuth,onAuthStateChanged } from "firebase/auth";
 import { app } from "./utils/firebase";
 import { useEffect } from "react";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil";
 import { userAtom } from "./store/atoms/user";
+import { TopBar } from "./components/TopBar";
 export const auth=getAuth(app)
 function App() {
-const [user,setUser]=useRecoilState(userAtom)
+ return <RecoilRoot>
+          <StoreApp/>
+       </RecoilRoot>
+}
 
-useEffect(()=>{
-onAuthStateChanged(auth,(user) => { 
-  if (user && user.email) { 
-   // User is signed in 
-   setUser({
-    loading:false,
-    user:{
-      email:user.email
-    }
-   })
-      console.log("User Signed In"); 
-      var uid = user.uid; 
-   } else { 
-      // User is signed out 
-      setUser({
-        loading:false
-      })
-      console.log("There is no logged-in User"); 
-      // ... 
-  } 
-});
-},[])
+function StoreApp(){
+  const [user, setUser]=useRecoilState(userAtom)
 
-  return (
-    <>
-      <div>
-      <Landing/>
-      <SignIn/>
-      </div>
-    </>
-  )
+  useEffect(()=>{
+  onAuthStateChanged(auth,(user) => { 
+    if (user && user.email) { 
+     // User is signed in 
+     setUser({
+      loading:false,
+      user:{
+        email:user.email
+      }
+     })
+        console.log("User Signed In"); 
+        //var uid = user.uid; 
+     } else { 
+        // User is signed out 
+        setUser({
+          loading:false
+        })
+        console.log("There is no logged-in User"); 
+        // ... 
+    } 
+  });
+  },[])
+  
+  
+  if(!user){
+    return <div>
+      <span>Login</span>
+    </div>
+  }
+  
+  if(user.loading){
+    <span>Loading...</span>
+  }
+    return (
+      <>
+        <div>
+          <TopBar/>
+          <span>You are logged in as {user.user?.email}</span>
+     
+        </div>
+      </>
+    )
 }
 
 export default App
