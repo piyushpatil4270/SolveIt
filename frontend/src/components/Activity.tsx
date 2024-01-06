e3abfed16af0804c07fc68e5b99c57ce34e372cc
Piyush <piyushpatil4270@gmail.com>
2024-01-06 21:38:06 +0530

    added the submission feature

--- frontend/src/components/Activity.tsx
+++ frontend/src/components/Activity.tsx
@@ -1,5 +1,30 @@
-export const Activity=()=>(
-    <div>
+import axios from "axios"
+import { useRecoilState } from "recoil"
+import { userAtom } from "../store/atoms/user"
+import { useEffect, useState } from "react"
+
+export const Activity=()=>{
+    const user=useRecoilState(userAtom)
+    const userEmail = user[0].user?.email;
+    const [submissions,setSubmissions]=useState<{answer:string}[]>()
+    const userSubmissions=async()=>{
+        const res=axios.post(`http://localhost:7000/api/problems/user/submissions`,{email:userEmail})
+        .then((res)=>{
+            setSubmissions(res.data)
+        })
+    }
+    useEffect(()=>{
+        userSubmissions()
+    },[])
+    return <div>
         <span>Activity</span>
+        {submissions?(
+        <div>
+            {submissions.map((submission)=>(
+                <span>{submission?.answer}</span>
+            ))}
+        </div>
+        ):("Loading...")}
+        {}
     </div>
-)
+}
--- frontend/src/components/SubmissionActivityList.tsx
+++
@@ -1,5 +0,0 @@
-export const SubmissionActivityList=()=>{
-    return <div>
-        
-    </div>
-}
