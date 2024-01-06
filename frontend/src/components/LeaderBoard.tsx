d6f58d0ec5294a11ebc50a5366b570415a01b93e
Piyush <piyushpatil4270@gmail.com>
2024-01-07 00:38:30 +0530

    added userimage in submission component

--- frontend/src/components/LeaderBoard.tsx
+++ frontend/src/components/LeaderBoard.tsx
@@ -11,6 +11,20 @@ export const LeaderBoard = () => {
         })
        
     }
+    const stringToColour = (str: string) => {
+        let hash = 0;
+        str.split('').forEach(char => {
+          hash = char.charCodeAt(0) + ((hash << 5) - hash)
+        })
+        let colour = '#'
+        for (let i = 0; i < 3; i++) {
+          const value = (hash >> (i * 8)) & 0xff
+          colour += value.toString(16).padStart(2, '0')
+        }
+        return colour
+      }
+      
+   
     useEffect(()=>{
         getLeaderboard()
     },[])
@@ -21,15 +35,26 @@ export const LeaderBoard = () => {
             <>
             <span className="text-[20px] ml-[10px] mt-[10px]">LeaderBoard</span>
             <div className="mx-[10px] flex flex-col gap-2">
-            {leaderboard?.map((user,i)=>(
-            <div className="flex  shadow-sm w-full h-[50px] items-center justify-start ">
+            {leaderboard?.map((user,i)=>{
+
+           let color=stringToColour(user.email)
+
+            return <div className="flex  shadow-sm w-full h-[50px] items-center justify-start ">
+                
                 <span className="w-[10%]" >{i+1}.</span>
-                <div className="flex w-[70%] justify-start">
+                <div className="w-[10%]">
+                
+                <div className={`w-[25px] h-[25px]  flex justify-center items-center rounded-full text-[#2c2c2c]`}  style={{backgroundColor:color}}>
+                {user.email[0]?.toUpperCase()}
+                </div>
+                </div>
+                <div className="flex xs:w-[50%] sm:w-[70%] justify-start">
+
                 <span className="">{user.email.split("@")[0]}</span>
                 </div>
-                <span className="w-[20%]">{user.points} points</span>
+                <span className="xs:w[40%] sm:w-[20%]">{user.points} points</span>
             </div>
-        ))}
+})}
         </div>
         </>
            ):(
--- frontend/src/components/charts/SubmissionChart.tsx
+++ frontend/src/components/charts/SubmissionChart.tsx
@@ -1,4 +1,16 @@
+import {ChartData,ChartOptions} from "chart.js"
+
 export const SubmissonChart=()=>{
+
+  /*import { Chart } from 'chart.js';
+    const myOptions: Chart.ChartOptions = {
+    title: {
+    display: true,
+    text: 'My title',
+    fontSize: 16,
+    },
+    };
+   */
     return(
         <div>
             SubmissionChart
