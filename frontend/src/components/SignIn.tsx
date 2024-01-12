a6ec95d7123e261ffb95359429a2925e5dcb3bb6
Piyush <piyushpatil4270@gmail.com>
2024-01-12 22:09:08 +0530

    commit

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -3,9 +3,22 @@ import {  signInWithPopup} from "firebase/auth";
 import { auth } from "../App";
 import cover from "../../src/assets/problem-solving.png"
 import gmailLogo from "../../src/assets/download (1).png"
+import { useState } from "react";
 const  provider= new GoogleAuthProvider()
 export const SignIn = () => {
+   
+  const [formData,setFormData]=useState({
+    email:"",
+    password:""
+  })
   
+  const handleChange=(value:any,name:any)=>{
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
+
   async function onSignIn() {
     signInWithPopup(auth, provider)
     .then((result) => {
@@ -32,7 +45,7 @@ export const SignIn = () => {
     }
 
    
-
+ console.log(formData)
   return <div className="w-[100%] h-full">
 {/*<button className="bg-green-700" onClick={()=>onSignIn()}>Sign-In</button>*/}
 <div className="w-full h-dvh flex">
@@ -40,13 +53,14 @@ export const SignIn = () => {
     <span className="text-transparent text-[45px] bg-clip-text bg-gradient-to-r from-[#4A1D96] to-[#8d76b6] font-medium">SolveIt</span>
   </div>
   <div className="sm:w-[70%] xs:w-full  bg-[#e8e8e8a7]  h-dvh flex flex-col justify-center items-center">
-  <div className="flex w-full  items-center justify-center">
+  <div className="flex w-full my-2  items-center justify-center ">
       <img src={cover} alt="logo" className="w-[60px] h-[60px] rounded-full " />
       <span className="text text-xl font-normal">SolveIt</span>
     </div>
     <div className="flex w-full flex-col items-center my-7 gap-5 justify-center">
-       <input className="w-[40%] outline-none h-8 rounded-sm" />
-       <input className="w-[40%] outline-none h-8 rounded-sm" />
+       <input placeholder="  Enter your email" className="xs:w-[60%] md:w-[40%] outline-none h-8 rounded-sm" value={formData.email} name="email" onChange={(e)=>{handleChange(e.target.value,"email")}} />
+       <input placeholder="  Enter your password" className="xs:w-[60%] md:w-[40%] outline-none h-8 rounded-sm" value={formData.password} name="password" onChange={(e)=>{handleChange(e.target.value,"password")}} />
+       <button className="w-fit h-fit py-1 px-3 bg-green-500 rounded-sm">Login</button>
     </div>
     <div className="w-[50%] h-[1px] bg-[#2e2e2e82]" />
     <div className="w-[45px] h-[45px] cursor-pointer flex  justify-center my-7 items-center bg-white rounded-full " onClick={()=>onSignIn()} >
